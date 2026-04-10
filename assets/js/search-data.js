304851b90f141ac6fa51f0fc8ac1432147c9a6b6
fracagnetta <71755398+fracagnetta@users.noreply.github.com>
2026-04-10 17:29:35 +0000

    Deploying to gh-pages from @ FLAREWorkshop/FLAREWorkshop.github.io@a2854bd74de369325a0f852ab2ace370c32ea416 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-schedule",
           title: "schedule",
-          description: "Tentative schedule for the FLARE Workshop, May 11–13 2026.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/schedule/";
